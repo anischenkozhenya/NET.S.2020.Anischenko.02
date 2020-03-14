584f45b797b08aa97a46534de3d534530b53a2c6
anischenkozhenya <43790223+anischenkozhenya@users.noreply.github.com>
2020-03-15 00:16:57 +0300

    +nextbiggermethod +testnextbiggesr +nugetnunit

---
+++ NET.S.2020.Anischenko.02/FindNextBiggerNumber/NumberFinder.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace FindNextBiggerNumber
+{
+    public static class NumberFinder
+    {
+        //Метод меняет элемент с индексом a и b местами
+        private static void Swap(ref char a, ref char b)
+        {
+            var temp = a;
+            a = b;
+            b = temp;
+        }
+        public static int? FindNextBiggerNumber(int number)
+        {
+            Stopwatch timerStopwatch= new Stopwatch();
+            timerStopwatch.Start();
+            int nextBiggerNumber;
+
+            if (number < 9 && number > 0)
+            {
+                return null;
+            }
+            if (number <= 0)
+                throw new ArgumentException();
+
+            //индеx цифры которое делает число больше
+            int n = 0;
+            //Массив символов преобразованный из входящего числа 
+            char[] num = number.ToString().ToCharArray();
+            for (int i = num.Length - 1; i >= 0; i--)
+            {
+                //такова числа нет
+                if (i == 0)
+                {
+                    return null;
+                }
+                if (num[i] > num[i - 1])
+                {
+                    n = i;
+                    break;
+                }
+            }
+            //цифра которую заменит ближайшая большая 
+            char x = num[n - 1];
+            //число большее чем x и меньшее следующее за x
+            int smallest = n;
+            for (int j = n; j < num.Length; j++)
+            {
+                if (num[j] > x && num[j] < num[smallest])
+                {
+                    smallest = j;
+                }
+            }
+            Swap(ref num[smallest], ref num[n - 1]);
+            bool checker = false;
+            //сортировка
+            do
+            {
+                for (int i = n + 1; i < num.Length; i++)
+                {
+                    if (num[i] < num[i - 1])
+                    {
+                        Swap(ref num[i], ref num[i - 1]);
+                        checker = true;
+                        break;
+                    }
+                    if (num[i] >= num[i - 1] && i == num.Length - 1)
+                    {
+                        checker = false;
+                    }
+                }
+            }while(checker);
+            try
+            {
+                nextBiggerNumber = checked(int.Parse(new string(num)));
+            }
+            catch (OverflowException ex)
+            {
+                throw new OverflowException();
+            }
+            timerStopwatch.Stop();
+            Console.WriteLine(timerStopwatch.Elapsed);
+            return nextBiggerNumber;
+        }
+    }
+}
---
+++ NET.S.2020.Anischenko.02/NumberFinderTests/NumberFinderTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using FindNextBiggerNumber;
+
+namespace NumberFinderTests
+{
+    [TestFixture]
+    public class NumberFinderTests
+    {
+        [TestCase(12, ExpectedResult = 21)]
+        [TestCase(513, ExpectedResult = 531)]
+        [TestCase(2017, ExpectedResult = 2071)]
+        [TestCase(414, ExpectedResult = 441)]
+        [TestCase(144, ExpectedResult = 414)]
+        [TestCase(1234321, ExpectedResult = 1241233)]
+        [TestCase(1234126, ExpectedResult = 1234162)]
+        [TestCase(3456432, ExpectedResult = 3462345)]
+        [TestCase(81957621, ExpectedResult = 81961257)]
+        [TestCase(10, ExpectedResult = null)]
+        [TestCase(20, ExpectedResult = null)] 
+        public int? NextBiggerThanTests(int number) => NumberFinder.FindNextBiggerNumber(number);
+
+    }
+}
