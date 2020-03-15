daaedc50dd638903de92dd90500cac4c74a1e575
anischenkozhenya <43790223+anischenkozhenya@users.noreply.github.com>
2020-03-15 19:34:03 +0300

    Add Filterdigit method and Filterdigittests

---
+++ NET.S.2020.Anischenko.02/FilterDigitTests/FilterDigitTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using NUnit.Framework;
+using FindNumberWithDigit;
+
+namespace FilterDigitTests
+{
+    [TestFixture]
+    public class FilterDigitTests
+    {
+        //[TestCase(7, 1, 2, 3, 4, 5, 6, 68, 69, 15, ExpectedResult = null)]
+        [TestCase(7, 1, 2, 3, 4, 5, 6, 7, 68, 69, 70, 15, 17, ExpectedResult = new int[]{7,70, 17})]
+        [TestCase(7, 1, 2, 3, 4, 5, 6, 68, 69,15,  ExpectedResult = new int[] { })]
+        [TestCase(7, 1, 2, 3, 4, 5, 6, 68, 69, int.MaxValue, ExpectedResult = new int[] {int.MaxValue })]
+        public int[] FilterDigitTest(int digit, params int[] numbers) => FinderNumberWithDigit.FilterDigit(digit, numbers);
+
+        [TestCase(-7, 1, 2, 3, 4, 5, 6, 7, 68, 69, 70, 15, 17)]
+        [TestCase(22, 1, 2, 3, 4, 5, 6, 68, 69, 15)]
+        public void DigitArgumentOutOfRangeException(int digit, params int[] numbers)
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => FinderNumberWithDigit.FilterDigit(digit, numbers));
+        }
+        [TestCase(1)]
+        [TestCase(5)]
+        public void DigitArgumentNullExceptions(int digit, params int[] numbers)
+        {
+            Assert.Throws<ArgumentNullException>(() => FinderNumberWithDigit.FilterDigit(digit, numbers));
+        }
+    }
+}
---
+++ NET.S.2020.Anischenko.02/FindNumberWithDigit/FinderNumberWithDigit.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FindNumberWithDigit
+{
+    public static class FinderNumberWithDigit
+    {
+        //public static int?[] FilterDigit(int digit, params int[] numbers)
+        /// <summary>
+        /// Returns array of numbers which contains digit
+        /// </summary>
+        /// <param name="digit">Digit</param>
+        /// <param name="numbers">Numbers</param>
+        /// <returns>Array of numbers which contains digit</returns>
+        public static int[] FilterDigit(int digit, params int[] numbers)
+        {
+            if (digit<0||digit>9)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            if (numbers.Length==0)
+            {
+                throw new ArgumentNullException();
+            }
+            var digitChar= digit.ToString();
+            var resultsNumbers = new List<int>();
+            foreach (var number in numbers)
+            {
+                if (number.ToString().Contains(digitChar))
+                {
+                    resultsNumbers.Add(number);
+                }
+            }
+            //return resultsNumbers.Count == 0 ? null : resultsNumbers.ToArray();
+            return resultsNumbers.ToArray();
+        } 
+    }
+}
