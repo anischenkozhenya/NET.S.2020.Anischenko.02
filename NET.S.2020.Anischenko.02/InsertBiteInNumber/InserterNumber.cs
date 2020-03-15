9cdd204bb51b3dba1f4239087f126ecd348f90e5
anischenkozhenya <43790223+anischenkozhenya@users.noreply.github.com>
2020-03-15 22:34:42 +0300

    create InsertBiteInNumber and InsertBiteInNumbertests

---
+++ NET.S.2020.Anischenko.02/InsertBiteInNumber/InserterNumber.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsertBiteInNumber
+{
+    /// <summary>
+    /// Inserts bits of the 2nd number starting from position startPosition to endPosition
+    /// to the 1st number.
+    /// </summary>
+    public static class InserterNumber
+    {
+        /// <summary>
+        /// Inserts bits of the 2nd number starting from position startPosition to endPosition
+        /// </summary>
+        /// <param name="numberSource"></param>
+        /// <param name="numberIn"></param>
+        /// <param name="startPositionBit"></param>
+        /// <param name="endPositionBit"></param>
+        /// <returns></returns>
+        public static int InsertNumber(int numberSource, int numberIn, int startPositionBit, int endPositionBit)
+        {
+            if (startPositionBit > 31 || startPositionBit < 0 || endPositionBit > 31 || endPositionBit < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            if (startPositionBit > endPositionBit)
+            {
+                throw new ArgumentException();
+            }
+            /*endPositionBit - startPositionBit   8-3 =5
+                *2<<5     00000000000000000010 => 000000000001000000    =64
+                *64-1 = 63   000000000000111111
+                *63<<3       000000000000111111000  504*/
+            int mask = ((2 << (endPositionBit - startPositionBit)) - 1) << startPositionBit;
+            /*  ~mask  111111111111000000111
+             firstNumber & ~mask  9 => 00000000001001 & 111111111111000000111=000000000000001
+             secondNumber << startPositionBit    00000000000001111 => 000000000000000001111000 120
+             000000000000001 | 000000001111000   =>  000000001111001 
+             */
+            return (numberSource & ~mask) | ((numberIn << startPositionBit) & mask);
+        }
+    }
+}
---
+++ NET.S.2020.Anischenko.02/InserterBiteInNumberTests/InserterNumberTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using InsertBiteInNumber;
+using System;
+
+namespace InserterBiteInNumberTests
+{
+    [TestFixture]
+    public class InserterNumberTests
+    {
+        [TestCase(9,15,3,8, ExpectedResult =121 )]
+        [TestCase(15, 15, 0, 0, ExpectedResult = 15)]
+        [TestCase(8, 15, 0, 0, ExpectedResult = 9)]
+        [TestCase(31, 9, 1, 4, ExpectedResult = 19)]
+        public int InsertNumberTest(int firstNumber,int secondNumber,int startPositionBit,int endPositionBite) => InserterNumber.InsertNumber(firstNumber,secondNumber,startPositionBit,endPositionBite);
+        
+        [TestCase(325,13,-1,3)]
+        [TestCase(325,13,-1,-5)]
+        public void StartOrEndBitLessZeroArgumentOutOfRangeException(int numberSource,int numberIn, int startPositionBit, int endPositionBit)
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() =>InserterNumber.InsertNumber(numberSource,numberIn,startPositionBit,endPositionBit));
+        }
+
+        [TestCase(234, 13, 2, 1)]
+        public void EndBitLessThenStartBitArgumentException(int numberSource, int numberIn, int startPositionBit, int endPositionBit)
+        {
+            Assert.Throws<ArgumentException>(() => InserterNumber.InsertNumber(numberSource, numberIn, startPositionBit, endPositionBit));
+        }
+    }
+}
